98a2f819108e075174ed231f6372f89f78db1007
krishav7k01 <64958980+krishav7k01@users.noreply.github.com>
2022-08-04 23:57:05 +0530

    Add files via upload

---
+++ Task4/CustomersService/Controllers/CustomerController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomersService.Models;
+
+namespace CustomersService.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        public static List<Customer> customerList = new List<Customer>()
+       { new Customer(){CustomerID=1,CustomerName="Krishav",CustomerContact=826509},
+           new Customer(){CustomerID=1,CustomerName="Keshav",CustomerContact=826809},
+           new Customer(){CustomerID=1,CustomerName="Aditi",CustomerContact=820809}
+       };
+
+        [HttpGet]
+        public List<Customer> Get()
+        {
+            return customerList;
+        }
+
+        [HttpGet("(id")]
+        public Customer Get(int id)
+        {
+            Customer obje = customerList.Find(item => item.CustomerID == id);
+                return obje;
+        }
+
+        [HttpPost]
+        public IActionResult Put([FromBody] Customer newobj)
+        {
+            foreach(Customer c in customerList)
+            {
+                if (newobj.CustomerID == c.CustomerID)
+                {
+                    c.CustomerID = newobj.CustomerID;
+                    c.CustomerName = newobj.CustomerName;
+                    c.CustomerContact = newobj.CustomerContact;
+                }
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            Customer conobj = customerList.Find(item => item.CustomerID == id);
+
+            if (customerList != null)
+            {
+                bool isRemoved = customerList.Remove(conobj);
+                if (isRemoved)
+                    return Ok(conobj);
+            }
+
+            return NotFound();
+        }
+
+
+
+       
+
+        
+     
+    }
+}
---
+++ Task4/CustomersService/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomersService.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public String CustomerName { get; set; }
+
+        public int CustomerContact { get; set; }
+    }
+}
